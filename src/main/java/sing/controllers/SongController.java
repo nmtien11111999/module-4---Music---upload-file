a7fbe284dc6284a2695ca96d47bce74f03848831
nmtien11111999 <nmtien11111999@email.com>
2024-06-17 20:18:11 +0700

    Music

--- src/main/java/sing/controllers/SongController.java
+++ src/main/java/sing/controllers/SongController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import sing.models.Song;
@@ -63,6 +60,36 @@ public class SongController {
         Song song = new Song(songForm.getId(), songForm.getName(),
                 songForm.getArtist(),songForm.getListMusic() , fileName);
         productService.addMusic(song);
+        return "redirect:/song/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView showEditForm(@PathVariable int id) {
+        Song song = productService.findById(id);
+        ModelAndView modelAndView = new ModelAndView("edit");
+        SongForm songForm = new SongForm(song.getId(), song.getName(), song.getArtist(), song.getListMusic(), null);
+        modelAndView.addObject("songForm", songForm);
+        modelAndView.addObject("lists", musicService.getListMusics());
+        return modelAndView;
+    }
+
+    @PostMapping("/edit")
+    public String updateProduct(@ModelAttribute SongForm songForm) {
+        MultipartFile multipartFile = songForm.getFileSong();
+        String fileName = multipartFile.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(songForm.getFileSong().getBytes(), new File(fileUpload + fileName));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        Song song = new Song(songForm.getId(), songForm.getName(), songForm.getArtist(), songForm.getListMusic(), fileName);
+        productService.update(songForm.getId(), song);
+        return "redirect:/song/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable int id) {
+        productService.delete(id);
         return "redirect:/song/list";
     }
 }
